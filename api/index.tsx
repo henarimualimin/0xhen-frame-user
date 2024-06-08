0ba217906b6e7955c09d193ee30b68f7ce83f1b2
Henari Mualimin <74995107+henarimualimin@users.noreply.github.com>
2024-06-08 08:00:20 +0700

    Update index.tsx

--- api/index.tsx
+++ api/index.tsx
@@ -83,7 +83,7 @@ app.frame('/', (c) => {
     intents: [
       <Button value="neynar">📍Home</Button>,
       <Button action="/neynar">🎩Stats</Button>,
-      <Button.Link href="https://warpcast.com/0xhen/0xe164f3c0">🎁Daily</Button.Link>,
+      <Button.Link href="https://warpcast.com/0xhen/0x1a8b7737">🎁Daily</Button.Link>,
       <Button.Link href="https://warpcast.com/~/compose?text=Frame_By%20@0xhen&embeds[]=https://0xhen-frame-user.vercel.app/api">🔍Share</Button.Link>,
     ],
   })
